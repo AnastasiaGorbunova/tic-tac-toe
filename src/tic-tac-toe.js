fe1ffd572234869f17edcc325cd95113cb789b95
Anastasia Gorbunova <nastya_gorbunova_1998@mail.ru>
2017-11-01 23:31:58 +0300

    first version

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,70 @@
 class TicTacToe {
     constructor() {
-
+        this.player = 'x';
+        this.field = [[null,null,null], [null,null,null], [null,null,null]];
+        this.turns = 0;
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.player;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (!this.field[rowIndex][columnIndex]) {
+            this.field[rowIndex][columnIndex] = this.player;
+            if (this.player === 'x') {
+                this.player = 'o';
+            }
+            else {
+                this.player = 'x';
+            }
+            this.turns++;
+        }
+        return this;
     }
 
     isFinished() {
-
+        if (this.noMoreTurns() || this.getWinner()){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     getWinner() {
-
+        for (var i = 0; i <= 2; i++) {
+            if (this.field[i][0] === this.field[i][1] && this.field[i][1] === this.field[i][2]){
+                return this.field[i][0];
+            }
+            if (this.field[0][i] === this.field[1][i] && this.field[1][i] === this.field[2][i]){
+                return this.field[0][i];
+            }
+        }
+        if ((this.field[0][0] === this.field[1][1] && this.field[1][1] === this.field[2][2]) || (this.field[0][2] === this.field[1][1] && this.field[1][1] === this.field[2][0])) {
+            return this.field[1][1];
+        }
+        return null;
     }
 
     noMoreTurns() {
-
+        if (this.turns === 9) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     isDraw() {
-
+        if (!this.isFinished() || this.getWinner()) {
+            return false;
+        }
+        return true;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex];
     }
 }
 
